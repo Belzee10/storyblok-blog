a8b65dff283317ee5d5f85394f07a19076895c36
Ivan <ivan.perez@lengoo.de>
2020-01-11 23:51:03 +0100

    clean up defaults

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,10 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
+    titleTemplate: 'StoryBlok Blog',
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
@@ -58,21 +56,7 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    customVariables: ['~/assets/variables.scss']
   },
   /*
    ** Build configuration
---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magni
+      voluptates ex assumenda excepturi repellendus ducimus neque minus
+      repellat, dolorum dignissimos ullam facilis eligendi aspernatur tempore
+      atque, placeat ea perspiciatis.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
